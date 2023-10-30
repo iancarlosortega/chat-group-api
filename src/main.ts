38e6efa64098330e19e5f529a1175c462bf658eb
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-10-30 11:25:12 -0500

    Project initialized

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Boostrap');
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(3000);
+  logger.log(`App running on port ${3000}`);
+}
+bootstrap();
