c1db37b68288fd78318a1d9b9703635ff8346f74
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-10-31 11:24:15 -0500

    Add connection to Postgres database

--- config/joi.validation.ts
+++ config/joi.validation.ts
@@ -1,5 +1,10 @@
 import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
-  PORT: Joi.number().default(5000),
+  DB_PASSWORD: Joi.string().default('postgres'),
+  DB_NAME: Joi.string().required(),
+  DB_HOST: Joi.string().default('localhost'),
+  DB_PORT: Joi.number().default(5432),
+  DB_USERNAME: Joi.string().default('postgres'),
+  PORT: Joi.number().default(4000),
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnvConfiguration } from 'config/app.config';
 import { JoiValidationSchema } from 'config/joi.validation';
 
@@ -9,6 +10,16 @@ import { JoiValidationSchema } from 'config/joi.validation';
     ConfigModule.forRoot({
       load: [EnvConfiguration],
       validationSchema: JoiValidationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     AuthModule,
   ],
