33d6fde4f55a6d9186c9b89e600b90379d336b67
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2023-10-30 22:02:56 -0500

    Add environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from 'config/app.config';
+import { JoiValidationSchema } from 'config/joi.validation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
